de7a410606032289873c257d9148a5889efc906a
Mike Rickher <mikat@cox.net>
2018-12-22 08:07:47 -0700

    add test winform, add copy to yaml

--- pipeTest/Form1.Designer.cs
+++ pipeTest/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPressMe = new System.Windows.Forms.Button();
+            this.lblResponse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to the Monkey Box";
+            // 
+            // btnPressMe
+            // 
+            this.btnPressMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPressMe.Location = new System.Drawing.Point(154, 132);
+            this.btnPressMe.Name = "btnPressMe";
+            this.btnPressMe.Size = new System.Drawing.Size(75, 56);
+            this.btnPressMe.TabIndex = 1;
+            this.btnPressMe.Text = "Press Me";
+            this.btnPressMe.UseVisualStyleBackColor = true;
+            this.btnPressMe.Click += new System.EventHandler(this.btnPressMe_Click);
+            // 
+            // lblResponse
+            // 
+            this.lblResponse.AutoSize = true;
+            this.lblResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResponse.Location = new System.Drawing.Point(158, 250);
+            this.lblResponse.Name = "lblResponse";
+            this.lblResponse.Size = new System.Drawing.Size(148, 37);
+            this.lblResponse.TabIndex = 2;
+            this.lblResponse.Text = "response";
+            this.lblResponse.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResponse);
+            this.Controls.Add(this.btnPressMe);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPressMe;
+        private System.Windows.Forms.Label lblResponse;
     }
 }
 
--- pipeTest/Form1.cs
+++ pipeTest/Form1.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +9,14 @@ namespace pipeTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void btnPressMe_Click(object sender, EventArgs e)
+        {
+            lblResponse.Text = "eee eech eeie";
+            lblResponse.Visible = true;
+            await Task.Delay(2000);
+            lblResponse.Visible = false;
         }
     }
 }

34c8be6b1e0ef2e314dd1101a3f69f90e866b693
Mike Rickher <mikat@cox.net>
2019-05-05 15:12:47 -0700

    test env params

--- artifactStagingReorganize.ps1
+++ artifactStagingReorganize.ps1
@@ -6,6 +6,9 @@ $build = $env:BUILD_CONFIGURATION
 #$folder = 'E:\test2'
 #$build = 'Release'
 
-Move-Item -Path $folder\pipeTest\bin\* -Destination $folder
-Remove-Item -Path $folder\pipeTest -Recurse
-Rename-Item -Path $folder\$build -NewName 'monkey'
+Write-Host "$folder"
+Write-Host "$build"
+
+#Move-Item -Path $folder\pipeTest\bin\* -Destination $folder
+#Remove-Item -Path $folder\pipeTest -Recurse
+#Rename-Item -Path $folder\$build -NewName 'monkey_app'

61aecf12b29bc474884bc08c126f643babf8cf53
Mike Rickher <mikat@cox.net>
2019-05-05 14:47:43 -0700

    ps for artifact

---
+++ artifactStagingReorganize.ps1
@@ -0,0 +1,11 @@
+﻿
+
+$folder = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+$build = $env:BUILD_CONFIGURATION
+
+#$folder = 'E:\test2'
+#$build = 'Release'
+
+Move-Item -Path $folder\pipeTest\bin\* -Destination $folder
+Remove-Item -Path $folder\pipeTest -Recurse
+Rename-Item -Path $folder\$build -NewName 'monkey'

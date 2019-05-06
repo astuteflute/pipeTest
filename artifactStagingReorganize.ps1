9948d857500de62fbeaf82012379bc414068403c
Mike Rickher <mikat@cox.net>
2019-05-05 21:54:00 -0700

    get-childitem

--- artifactStagingReorganize.ps1
+++ artifactStagingReorganize.ps1
@@ -7,6 +7,11 @@ $build = $env:BUILDCONFIGURATION
 Write-Host "folder $folder"
 Write-Host "build $build"
 
+echo Get-ChildItem -Path $folder -Recurse
+
 echo Move-Item -Path $folder\pipeTest\bin\$build -Destination $folder 
 echo Remove-Item -Path $folder\pipeTest -Recurse
 echo Rename-Item -Path $folder\$build -NewName 'monkey_app'
+
+echo Get-ChildItem -Path $folder -Recurse
+

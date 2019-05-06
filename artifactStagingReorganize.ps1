f351f883b83656669d85881b87be18e518ee1bd7
Mike Rickher <mikat@cox.net>
2019-05-06 08:46:51 -0700

    after retest on local

--- artifactStagingReorganize.ps1
+++ artifactStagingReorganize.ps1
@@ -1,15 +1,30 @@
-﻿
+﻿# Enable -Verbose option
+[CmdletBinding()]
+
 $folder = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 $build = $env:BUILDCONFIGURATION
+
+#$folder = 'E:\test2\Release20190506.7'
+#$build = 'Release'
 
 Write-Host "folder $folder"
 Write-Host "build $build"
 
-echo Get-ChildItem -Path $folder -Recurse
+$dir = Get-ChildItem -Path $folder -Recurse
+foreach ($obj in $dir) {
+    Write-Host "$($obj.Parent.FullName)  $($obj.DirectoryName)  $($obj.Name)"
+}
 
-echo Move-Item -Path $folder\pipeTest\bin\$build -Destination $folder 
-echo Remove-Item -Path $folder\pipeTest -Recurse
-echo Rename-Item -Path $folder\$build -NewName 'monkey_app'
+Write-Host '**'
+Write-Host 'Change files'
+Write-Host '**'
 
-echo Get-ChildItem -Path $folder -Recurse
+Move-Item -Path $folder\pipeTest\bin\$build -Destination $folder 
+Remove-Item -Path $folder\pipeTest -Recurse
+Rename-Item -Path $folder\$build -NewName 'monkey_app'
+
+$dir = Get-ChildItem -Path $folder -Recurse
+foreach ($obj in $dir) {
+    Write-Host "$($obj.Parent.FullName)  $($obj.DirectoryName)  $($obj.Name)"
+}
 

c6aff50d27888eae78bbf80685be756a83b338d3
Mike Rickher <mikat@cox.net>
2019-05-05 20:20:51 -0700

    no wildcard

--- artifactStagingReorganize.ps1
+++ artifactStagingReorganize.ps1
@@ -7,6 +7,6 @@ $build = $env:BUILDCONFIGURATION
 Write-Host "folder $folder"
 Write-Host "build $build"
 
-echo Move-Item -Path $folder\pipeTest\bin\* -Destination $folder -WhatIf
+echo Move-Item -Path $folder\pipeTest\bin\$build -Destination $folder 
 echo Remove-Item -Path $folder\pipeTest -Recurse
 echo Rename-Item -Path $folder\$build -NewName 'monkey_app'
